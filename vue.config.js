3cf333a2abbc7ef7ff217a1bc0a1bf815f102910
Valentin von Guttenberg <greengiraffe@posteo.de>
2019-11-03 23:42:26 +0100

    :bug: Fix sass loader options
    Dependency upgrades broke the css-loader options in vue.config.js, that provided variables and mixins access for all files. This is solved by using style-resources-loader instead.

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,27 @@
+const path = require('path')
+
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        data: `@import "@/styles/_variables.scss"; @import "@/styles/_mixins.scss";`
-      }
-    }
-  },
   pwa: {
-    name: "Wingspan Helper",
-    themeColor: "#cae6f3",
-    msTileColor: "#cae6f3",
-    assetsVersion: "1.0",
+    name: 'Wingspan Helper',
+    themeColor: '#cae6f3',
+    msTileColor: '#cae6f3',
+    assetsVersion: '1.0',
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
       appleTouchIcon: 'img/icons/icon-152x152.png',
       maskIcon: 'img/icons/maskable-icon.svg',
       msTileImage: 'img/icons/icon-144x144.png'
+    }
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/styles/_variables.scss'),
+        path.resolve(__dirname, 'src/styles/_mixins.scss')
+      ]
     }
   }
 }
