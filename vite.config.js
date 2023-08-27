301622d57c92d8593345ab949dec139cbf75b026
Valentin von Guttenberg <greengiraffe@posteo.de>
2023-08-27 16:37:39 +0200

    ✨ Re-introduce PWA support via vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import vue from "@vitejs/plugin-vue";
 import {fileURLToPath, URL} from "url";
 import {defineConfig} from "vite";
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,77 @@ export default defineConfig({
     vueI18n({
       compositionOnly: false,
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}']
+      },
+      manifest: {
+        name: 'Wingspan Helper',
+        short_name: 'Wingspan Helper',
+        description: 'A digital scoresheet for the Wingspan board game',
+        theme_color: '#cae6f3',
+        icons: [
+          {
+            src: 'img/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'img/icons/maskable-icon.svg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ]
+      },
+    })
   ],
   resolve: {
     alias: {
